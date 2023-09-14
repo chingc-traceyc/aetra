73be33b2cc4fa955cadfef9d193e6b5fe4ea0ac3
tracey chung <tchung97@gmail.com>
2023-09-14 13:19:50 -0700

    Setting up

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const pool = require('./db')
+const port = 1337
+
+const app = express()
+app.use(express.json())
+
+// routes
+app.get('/', async (req, res) => {
+    try {
+        await pool.query('SELECT * FROM schools')
+        res.status(200).send(data.rows)
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500)
+    }
+})
+
+app.post('/', async (req, res) => {
+    const { name, location } =  req.body
+    try {
+        await pool.query('INSERT INTO schools(name, address) VALUES ($1, $2)', [name,location])
+        res.status(200).send({message: "Successfully added child"})
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500)
+    }
+})
+    // const { name, location } =  req.body
+    // res.status(200).send({
+    //     message: `YOUR KEYS WERE ${name}, ${location}`
+    // })
+
+
+app.get('/setup', async (req, res) => {
+    try {
+        await pool.query('CREATE TABLE schools(id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))')
+        res.status(200).send({ message: "Successfully created table" });
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500)
+    }
+})
+
+app.listen(port, () => console.log(`Server has started on port: ${port}`))
